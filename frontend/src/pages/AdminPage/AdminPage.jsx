7e6968bc7da0bcb0a1f48ca4528dd038671b8806
Ahmad Rammal <ahmad.rammal2023@gmail.com>
2023-12-04 00:03:48 +0200

    Update Admin Page

--- frontend/src/pages/AdminPage/AdminPage.jsx
+++ frontend/src/pages/AdminPage/AdminPage.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import Navbar from '../../components/adminPage/navbar/navbar'
 import ManageDoctors from '../../components/adminPage/ManageDoctors/ManageDoctors'
 import ManagePatients from '../../components/adminPage/ManagePatients/ManagePatients'
 
 function AdminPage() {
+  const [manageDoctors , setManageDoctors] = useState(false)
+  const [managePatients , setManagedPatients] = useState(false)
+
+  const openPatientsSection = () => {
+    setManagedPatients(!managePatients)
+    setManageDoctors(false)
+  }
+
+  const openDoctorsSection = () => {
+    setManagedPatients(false)
+    setManageDoctors(!manageDoctors)
+  }
+
   return (
     <div>
-      <Navbar />
-      {/* <ManageDoctors /> */}
-      <ManagePatients />
+      <div className="flex nav-container">
+        <div className={manageDoctors ? 'nav-item selected' : 'nav-item'} onClick={openDoctorsSection} >Manage Doctors</div>
+        <div className={managePatients ? 'nav-item selected' : 'nav-item'} onClick={openPatientsSection}>Manage Patients</div>
+        <div className="nav-item">Rooms Reservations</div>
+      </div>
+
+      {manageDoctors ? <ManageDoctors /> : ''}
+      {managePatients ? <ManagePatients /> : ''}
     </div>
   )
 }
